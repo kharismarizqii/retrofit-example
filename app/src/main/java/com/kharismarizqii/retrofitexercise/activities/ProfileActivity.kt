d2225b1036931c9bcb461be0bfd49817a8bf8296
kharismarizqii <rizqikharisma@gmail.com>
2020-04-17 18:53:11 +0700

    add bottom nav and fragment

--- app/src/main/java/com/kharismarizqii/retrofitexercise/activities/ProfileActivity.kt
+++ app/src/main/java/com/kharismarizqii/retrofitexercise/activities/ProfileActivity.kt
@@ -3,19 +3,31 @@ package com.kharismarizqii.retrofitexercise.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kharismarizqii.retrofitexercise.R
+import com.kharismarizqii.retrofitexercise.fragments.HomeFragment
+import com.kharismarizqii.retrofitexercise.fragments.SettingsFragment
+import com.kharismarizqii.retrofitexercise.fragments.UserFragment
 import com.kharismarizqii.retrofitexercise.storage.SharedPrefManager
 import kotlinx.android.synthetic.main.activity_profile.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        val user = SharedPrefManager.getInstance(this).user
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        displayFragment(HomeFragment())
+    }
 
-        tvWMsg.text = "Welcome Back ${user.name}"
+    private fun displayFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.layoutplacer, fragment)
+            .commit()
     }
 
     override fun onStart() {
@@ -26,5 +38,25 @@ class ProfileActivity : AppCompatActivity() {
 
             startActivity(intent)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when(item.itemId){
+            R.id.menu_home -> {
+                fragment = HomeFragment()
+            }
+            R.id.menu_user -> {
+                fragment = UserFragment()
+            }
+            R.id.menu_settings -> {
+                fragment = SettingsFragment()
+            }
+        }
+
+        if (fragment!= null){
+            displayFragment(fragment)
+        }
+        return false
     }
 }
